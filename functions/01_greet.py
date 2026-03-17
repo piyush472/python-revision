a0df28bf828a904fe77f2d921627314dd2c3bf57
Piyush <piyushnegi310@gmail.com>
2026-03-17 23:35:27 +0530

    check something

--- functions/01_greet.py
+++ functions/01_greet.py
@@ -1,5 +1,5 @@
 ﻿# Question 1: Greet function
 # Write a function greet(name) that prints "Hello, <name>".
 def greet(name):
-    
+    return name
 print(greet("hello user!"))
--- functions/17_sum_digits.py
+++ functions/17_sum_digits.py
@@ -1,9 +1,2 @@
 ﻿# Question 17: Sum of digits
 # Write a function sum_digits(n) that returns sum of digits of a number.
-def sum_digits(n):
-    # your code here
-    pass
-
-if __name__ == "__main__":
-    # Test your function here
-    pass
